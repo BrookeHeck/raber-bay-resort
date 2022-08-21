3dd9d0bc51cfda0283627e2283ff5e4f30177e6f
Brooke Heck <b19heck@gmail.com>
2022-08-20 19:41:14 -0700

    react app working

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Header>
+        <h1>Hello From My App</h1>
+      </Header>
+    );
+  }
+}
+
+export default Header;
