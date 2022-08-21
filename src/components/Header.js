30e0c9ac49ee8b6b0b6c08d5ca2a88fac1eabdb3
Brooke Heck <b19heck@gmail.com>
2022-08-20 20:13:14 -0700

    add a boostrap header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import '../scss/Header.scss';
+import Nav from 'react-bootstrap/Nav';
 
 class Header extends React.Component {
   render() {
     return (
-      <h1>Hello From My App</h1>
+      <header>
+        <h1>Raber Bay Resort</h1>
+        <Nav activeKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Cabins</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Contact Us</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </header>
     );
   }
 }
